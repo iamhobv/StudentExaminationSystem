9ebd020491cf472b87e59c0cf93de654d069ca0d
areege16 <areege162002@gmail.com>
2025-05-18 16:46:19 +0300

    t

--- DTOs/ExamDTOs/updateExamDTO.cs
+++ DTOs/ExamDTOs/updateExamDTO.cs
@@ -2,9 +2,9 @@
 {
     public class updateExamDTO
     {
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public TimeOnly? Duration { get; set; }
 
-        public List<int> QuestionIds { get; set; }
+        public List<ExamQuestionDTO>? ExamQuestions { get; set; }
     }
 }
