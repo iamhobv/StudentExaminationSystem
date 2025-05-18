abf000f1ae127791d5b76759799ba848aceddabb
HosamGirgis55 <hosamgirgis55@gmail.com>
2025-05-18 16:01:47 +0300

    finsh student

---
+++ DTOs/QuestionDTOs/QuestionDTO.cs
@@ -0,0 +1,16 @@
+﻿using StudentExamSystem.Models;
+using static StudentExamSystem.Enums.QuestionType;
+
+namespace StudentExamSystem.DTOs.QuestionDTOs
+{
+    public class QuestionDTO : BaseModel
+    {
+        public string QuestionBody { get; set; }
+        public QuestionTypes QuestionType { get; set; }
+        public int QuestionMark { get; set; }
+        public string CorrectAnswer { get; set; }
+
+        public string StudentQuestionAnswer { get; set; }
+
+    }
+}
